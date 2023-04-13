fb9a7b03ec1d7b5829cfc6f34ea1718246151cd0
Harshit M <harshitmeh111@gmail.com>
2023-04-13 21:46:40 +0530

    Added code

---
+++ server.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request
+import sqlite3
+
+class Contacts:
+    def __init__(self):
+        self.__my_conn = sqlite3.connect('db/contacts.db')
+        self.__my_curr = self.__my_conn.cursor()
+    
+    def put_data(self, name, number):
+        self.__my_curr.execute(f'insert into identity values ("{name}", {number})')
+        self.__my_conn.commit()
+    
+    def close_conn(self):
+        self.__my_conn.close()
+
+    def get_data(self):
+        self.__my_curr.execute('select * from identity')
+        return self.__my_curr.fetchall()
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home_page():
+    if request.method == 'GET':
+        obj = Contacts()
+        all_data = obj.get_data()
+        obj.close_conn()
+
+        return render_template('index.html', all_data=all_data)
+
+    elif request.method == 'POST':
+        name = request.form.get('name')
+        number = request.form.get('number')
+        obj = Contacts()
+        obj.put_data(name, number)
+        obj.close_conn()
+
+        return render_template('success.html')
+ 
+if __name__ == '__main__':
+    app.run(debug=True)
