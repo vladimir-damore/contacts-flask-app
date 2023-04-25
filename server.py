a28a8e1890eba5eb30635a6043aa3e46be6d8f71
Harshit M <harshitmeh111@gmail.com>
2023-04-25 22:34:20 +0530

    Make delete button

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import sqlite3
 
 class Contacts:
@@ -13,6 +13,10 @@ class Contacts:
     def get_data(self):
         self.__my_curr.execute('select * from identity')
         return self.__my_curr.fetchall()
+    
+    def delete_data(self, name):
+        self.__my_curr.execute(f'delete from identity where name="{name}"')
+        self.__my_conn.commit()
 
     def close_conn(self):
         self.__my_conn.close()
@@ -33,14 +37,19 @@ def home_page():
         number = request.form.get('number')
         obj = Contacts()
         obj.put_data(name, number)
+        all_data = obj.get_data()
         obj.close_conn()
 
-        return render_template('success.html')
- 
-@app.route('/delete', methods=['GET', 'POST'])
-def delete_data():
-    if request.method == 'GET':
-        return render_template('success.html')
+        return redirect('/')
+
+@app.route('/delete/<string:name>')
+def delete_data(name):
+    obj = Contacts()
+    obj.delete_data(name)
+    all_data = obj.get_data()
+    obj.close_conn()
+
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
