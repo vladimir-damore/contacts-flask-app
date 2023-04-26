b6699298d3105decd2e0e405ccb95577ea8f7de1
Harshit M <harshitmeh111@gmail.com>
2023-04-26 18:04:26 +0530

    Added the update button

--- server.py
+++ server.py
@@ -6,19 +6,27 @@ class Contacts:
         self.__my_conn = sqlite3.connect('db/contacts.db')
         self.__my_curr = self.__my_conn.cursor()
     
-    def put_data(self, name, number):
+    def put_data(self, name:str, number:int) -> None:
         self.__my_curr.execute(f'insert into identity values ("{name}", {number})')
         self.__my_conn.commit()
 
-    def get_data(self):
+    def get_all_data(self) -> list:
         self.__my_curr.execute('select * from identity')
         return self.__my_curr.fetchall()
     
-    def delete_data(self, name):
+    def get_data_from_name(self, name:str) -> int:
+        self.__my_curr.execute(f'select number from identity where name="{name}"')
+        return self.__my_curr.fetchone()
+    
+    def delete_data_from_name(self, name:str) -> None:
         self.__my_curr.execute(f'delete from identity where name="{name}"')
         self.__my_conn.commit()
 
-    def close_conn(self):
+    def update_data(self, name:str, number:int) -> None:
+        self.__my_curr.execute(f'update identity set number={number} where name="{name}"')
+        self.__my_conn.commit()
+
+    def close_connection(self) -> None:
         self.__my_conn.close()
 
 app = Flask(__name__)
@@ -27,8 +35,8 @@ app = Flask(__name__)
 def home_page():
     if request.method == 'GET':
         obj = Contacts()
-        all_data = obj.get_data()
-        obj.close_conn()
+        all_data = obj.get_all_data()
+        obj.close_connection()
 
         return render_template('index.html', all_data=all_data)
 
@@ -37,19 +45,36 @@ def home_page():
         number = request.form.get('number')
         obj = Contacts()
         obj.put_data(name, number)
-        all_data = obj.get_data()
-        obj.close_conn()
+        obj.close_connection()
 
         return redirect('/')
 
 @app.route('/delete/<string:name>')
-def delete_data(name):
+def delete_data(name):  
     obj = Contacts()
-    obj.delete_data(name)
-    all_data = obj.get_data()
-    obj.close_conn()
+    obj.delete_data_from_name(name)
+    obj.close_connection()
 
     return redirect('/')
+
+@app.route('/update/<string:name>', methods=['GET', 'POST'])
+def update_data(name):
+    if request.method == 'GET':
+        print('get')
+        obj = Contacts()
+        number = obj.get_data_from_name(name)[0]
+        obj.close_connection()
+
+        return render_template('update.html', name=name, number=number)
+    
+    elif request.method == 'POST':
+        number = request.form.get('number')
+        print(name, number)
+        obj = Contacts()
+        obj.update_data(name, number)
+        obj.close_connection()
+
+        return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
